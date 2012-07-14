862e2e4abe1044382de73b0233e4c8807929a4f4
Dotan J. Nahum <jondotan@gmail.com, dotan@paracode.com>
2012-07-15 00:54:18 +0300

    initial import

---
+++ roles/appserver.rb
@@ -0,0 +1,31 @@
+
+name "appserver"
+description "Base appserver"
+run_list "recipe[apt]",
+		 "recipe[git]",
+		 "recipe[java]",
+		 "recipe[imagemagick]",
+		 "recipe[ruby_build]", 
+		 "recipe[rbenv::system]", 
+		 "recipe[nodejs]",
+		 "recipe[nodejs-support]",
+		 "recipe[nginx]"
+		 
+override_attributes({
+  'rbenv' => {
+	'rubies' => [ "1.9.3-p194", "jruby-1.6.7.2" ],
+	'gems'   => {
+		"1.9.3-p194" => [
+			{'name'=> 'bundler', 'version'=>'1.1.4'}
+		],
+		"jruby-1.6.7.2" => [
+			{'name'=>'bundler', 'version'=>'1.1.4'}
+		]
+	}
+  },
+  
+  'java' => {
+	'jdk_version' => '7'
+  }
+})
+
